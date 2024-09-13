701e7866724dd828db7a3897466e1448cf63270d
Rock6982 <rodrigoqm75@gmail.com>
2024-09-13 03:48:39 -0400

    se creo la clase POKEMON

---
+++ js/cardPokemon.js
@@ -0,0 +1,12 @@
+class Pokemon {
+  constructor(name, type, abilities) {
+    this.name = name;
+    this.type = type;
+    this.abilities = abilities;
+  }
+  createHTML() {
+    const cardHTML = ``;
+
+    return cardHTML;
+  }
+}
