646f885594432224bee383a2bad04cfb11bb226a
Rock6982 <rodrigoqm75@gmail.com>
2024-09-16 16:38:20 -0400

    update

--- js/cardPokemon.js
+++ js/cardPokemon.js
@@ -1,12 +1,39 @@
 class Pokemon {
-  constructor(name, type, abilities) {
+  constructor(img, name, type, abilities) {
+    this.img = img;
     this.name = name;
     this.type = type;
     this.abilities = abilities;
   }
-  createHTML() {
-    const cardHTML = ``;
+  createHTML(index) {
+    let cardHTML = `
+    <div class="content-card">
+          <div class="content-img">
+            <img
+              class="img"
+              src="${this.img}"
+              alt=""
+            />
+          </div>
+          <h3 class="name content">${this.name}</h3>
+          <div class="content-type">
+            <h4>Type :</h4>`;
+    this.type.forEach((element) => {
+      cardHTML += `<p class="content">${element}</p>`;
+    });
+
+    cardHTML += `</div>
+          <div class="content-abilities">
+            <h4>Abilities :</h4>`;
+    this.abilities.forEach((element) => {
+      cardHTML += `<p class="content">${element}</p>`;
+    });
+    cardHTML += `</div>
+    </div>
+    `;
 
     return cardHTML;
   }
 }
+
+export default Pokemon;
